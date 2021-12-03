5d4ac8133b98c7cc84f4006cb3201d71ba955fd8
Martin P <martimuseq@outlook.com>
2021-12-03 14:21:42 +0100

    footer changes

--- frontend/src/Footer.js
+++ frontend/src/Footer.js
@@ -11,6 +11,17 @@ import "bootstrap/dist/css/bootstrap.min.css";
 // https://stackoverflow.com/questions/39999367/how-do-i-reference-a-local-image-in-react
 // Myk z import działa.
 const nazwa_strony = "HIRD GYLDA HIRTIR";
+const url = "https://www.linkedin.com/in/martin-m-permus/";
+const link_linkedin = <a href={url}>M. M. Permus</a>;
+
+//https://www.30secondsofcode.org/react/s/mailto
+const Mailto = ({ email, subject = "", body = "", children }) => {
+	let params = subject || body ? "?" : "";
+	if (subject) params += `subject=${encodeURIComponent(subject)}`;
+	if (body) params += `${subject ? "&" : ""}body=${encodeURIComponent(body)}`;
+
+	return <a href={`mailto:${email}${params}`}>{children}</a>;
+};
 
 export function Footer() {
 	return (
@@ -18,14 +29,40 @@ export function Footer() {
 			<Navbar fixed="bottom" bg="dark">
 				<Grid container spacing={2}>
 					<Grid item xs={4}>
-						<img src={logo} width="50" height="50" alt="" />
+						<Grid container spacing={2}>
+							<Grid item xs={12}>
+								<p>link1</p>
+							</Grid>
+							<Grid item xs={12}>
+								<p>link1</p>
+							</Grid>
+							<Grid item xs={12}>
+								<p>link1</p>
+							</Grid>
+						</Grid>
 					</Grid>
-					<Grid item xs={8}>
-						<Typography variant="h3" gutterBottom component="div">
-							{nazwa_strony}
+					<Grid item xs={4}>
+						<img src={logo} width="150" height="150" alt="" />
+					</Grid>
+					<Grid item xs={4}>
+						<Typography variant="h6" gutterBottom component="div">
+							Napisz do nas:
+						</Typography>
+						<Typography variant="h6" gutterBottom component="div">
+							<Mailto
+								email="gyldahirtir@gmail.com"
+								subject=""
+								body=""
+							>
+								gyldahirtir@gmail.com
+							</Mailto>
 						</Typography>
 					</Grid>
 				</Grid>
+
+				<Typography variant="h6" gutterBottom component="div">
+					Made with 💛 by {link_linkedin}
+				</Typography>
 			</Navbar>
 		</>
 	);
